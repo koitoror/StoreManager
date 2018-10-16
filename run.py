82dc578f546bb361700e02295b2a91a64b91b054
Daniel Kamar <kamarster@gmail.com>
2018-10-17 02:38:14 +0300

    [Chore #161269373] Automate tests integration and run

---
+++ run.py
@@ -0,0 +1,33 @@
+""""Application Product Point."""
+
+import os
+import unittest
+
+# third-party imports
+from flask_script import Manager # controller class for handling commands
+
+# local imports
+from app.api.v1 import create_app
+from app import api_v1
+
+# application development instance
+app = create_app(config_name=os.getenv("FLASK_CONFIG"))
+
+# registering the blueprint
+app.register_blueprint(api_v1)
+
+# initializing the manager object
+manager = Manager(app)
+
+@manager.command
+def run():
+    app.run()
+
+@manager.command
+def test():
+    test = unittest.TestLoader().discover("./app/tests", pattern="test*.py")
+    unittest.TextTestRunner(verbosity=2).run(test)
+
+
+if __name__ == "__main__":
+    manager.run()
