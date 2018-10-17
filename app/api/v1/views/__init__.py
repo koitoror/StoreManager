7c12956ee058590f42f60196a7b8f15972f6ad64
Daniel Kamar <kamarster@gmail.com>
2018-10-17 11:56:57 +0300

    [bug #161269373] Refactor namespaces

--- app/api/v1/views/__init__.py
+++ app/api/v1/views/__init__.py
@@ -1,41 +1,41 @@
-from flask_restplus import Api
-from flask import Blueprint
+# from flask_restplus import Api
+# from flask import Blueprint
 
-from .products_views import api as products_ns
-from .sales_views import api as sales_ns
+# from .products_views import api as products_ns
+# from .sales_views import api as sales_ns
 
 
-# blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
+# # blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
+
+# # api = Api(
+# #     blueprint,
+# #     title='StoreManager',
+# #     doc='/api/documentation',
+# #     version='1.0',
+# #     description='StoreManager is a web application that helps store owners manage sales and product inventory records. This application is meant for use in a single store.'
+# # )
+
+# api_v1 = Blueprint('api', __name__, url_prefix='/api/v1')
+
+# authorizations = {
+#     'apikey' : {
+#         'type' : 'apiKey',
+#         'in' : 'header',
+#         'name' : 'X-API-KEY'
+#     }
+# }
 
 # api = Api(
-#     blueprint,
-#     title='StoreManager',
-#     doc='/api/documentation',
+#     api_v1, 
+#     title='StoreManager API :: v1',
+#     doc='/documentation',
 #     version='1.0',
-#     description='StoreManager is a web application that helps store owners manage sales and product inventory records. This application is meant for use in a single store.'
+#     authorizations=authorizations,
+#     description='StoreManager is a web application that helps store owners manage sales and product inventory records. This application is meant for use in a single store. A simple StoreManager API',
 # )
 
-api_v1 = Blueprint('api', __name__, url_prefix='/api/v1')
+# api.add_namespace(products_ns)
+# api.add_namespace(sales_ns)
 
-authorizations = {
-    'apikey' : {
-        'type' : 'apiKey',
-        'in' : 'header',
-        'name' : 'X-API-KEY'
-    }
-}
-
-api = Api(
-    api_v1, 
-    title='StoreManager API :: v1',
-    doc='/documentation',
-    version='1.0',
-    authorizations=authorizations,
-    description='StoreManager is a web application that helps store owners manage sales and product inventory records. This application is meant for use in a single store. A simple StoreManager API',
-)
-
-api.add_namespace(products_ns)
-api.add_namespace(sales_ns)
-
-# api.add_namespace(products_ns, path='/api/v1')
-# api.add_namespace(sales_ns, path='/api/v1')
+# # api.add_namespace(products_ns, path='/api/v1')
+# # api.add_namespace(sales_ns, path='/api/v1')
